bd2a62ec6d3a4bff192c5b187b5a2a383c587000
jose felipe da silva santos <josefelipesilvasantos874@gmail.com>
2022-03-18 20:39:32 -0300

    finaliza exercicio

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int n1, n2, Resultado;
+
+Console.WriteLine("Cálculo da soma entre dois números.\n"); 
+
+Console.Write("Digite o primeiro número:"); 
+n1 = Convert.ToInt32(Console.ReadLine()!);
+
+Console.Write("Digite o segundo número:"); 
+n2 = Convert.ToInt32(Console.ReadLine()!);
+
+Resultado = n1 + n2; 
+Console.WriteLine($"soma: {Resultado}");
+
+
